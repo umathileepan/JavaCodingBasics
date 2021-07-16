a06008e1930ca377caad3ad03700cd45abc69569
Uma Thileepan <thileepan_uma@yahoo.co.uk>
2021-07-16 18:32:09 +0100

    Control flow is used with out methods.

--- src/com/uma/Main.java
+++ src/com/uma/Main.java
@@ -13,18 +13,48 @@ public class Main {
 	    final byte PERCENT = 100;
 
 	    Scanner scanner = new Scanner(System.in);
+        int principal;
+        float annualInterestRate;
+        float monthlyInterestRate;
+        byte years;
+        int numberOfPayments;
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        while(true) {
+            System.out.print("Principal($1K-1M): ");
+            principal = scanner.nextInt();
+            if(principal >= 1000 && principal <= 1_000_000 ) {
+                break;
+            }
+            else{
+                System.out.println("Enter a number between 1_000 and 1_000_000");
+            }
 
-        System.out.print("Annual Interest Rate: ");
-        float annualInterestRate = scanner.nextFloat();
-        float monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
+        }
 
-        System.out.print("Period (Years) : ");
-        byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTHS_IN_YEAR;
+        while(true) {
+            System.out.print("Annual Interest Rate: ");
+            annualInterestRate = scanner.nextFloat();
+            if(annualInterestRate >= 1 && annualInterestRate <= 30) {
+                monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
+                break;
+            }
+            else {
+                System.out.println("Enter a value greater than 0 and less than or equal 30.");
 
+            }
+        }
+
+        while(true) {
+            System.out.print("Period (Years) : ");
+            years = scanner.nextByte();
+            if(years >= 1 && years <= 30) {
+                numberOfPayments = years * MONTHS_IN_YEAR;
+                break;
+            }
+            else{
+                System.out.println("Enter a value between 1 and 30");
+            }
+        }
 
         double mortgage = principal*(monthlyInterestRate*Math.pow(1+monthlyInterestRate,numberOfPayments)/Math.pow(1+monthlyInterestRate,numberOfPayments)-1);
         String result = NumberFormat.getCurrencyInstance().format(mortgage);
