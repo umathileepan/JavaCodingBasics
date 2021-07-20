95eb0e2c30b46d0fcf71ecc320179904584b244a
Uma Thileepan <thileepan_uma@yahoo.co.uk>
2021-07-20 08:59:13 +0100

    Playing with Generics.

---
+++ src/com/uma/Generics/GenericList.java
@@ -0,0 +1,14 @@
+package com.uma.Generics;
+
+public class GenericList<T> {
+    private T[] items =  (T[]) new Object[10];
+    private int count;
+
+    public void add(T item){
+        items[count++] = item;
+    }
+
+    public T get(int index){
+        return items[index];
+    }
+}
--- src/com/uma/Main.java
+++ src/com/uma/Main.java
@@ -1,19 +1,27 @@
 package com.uma;
 
+import com.uma.Generics.GenericList;
+import com.uma.Generics.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("Hello World");
 
-        int principal = (int) Console.readNumber("Principal($1K-1M): ", 1_000, 1_000_000);
-        float annualInterestRate = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
-        byte years = (byte) Console.readNumber("Period (Years) : ", 1, 30);
+        var list = new GenericList<Integer>();
+        list.add(5);
+        list.add(10);
 
-        var calculator = new MortgageCalculator(principal,annualInterestRate,years);
+         int principal = (int) Console.readNumber("Principal($1K-1M): ", 1_000, 1_000_000);
+         float annualInterestRate = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
+         byte years = (byte) Console.readNumber("Period (Years) : ", 1, 30);
 
-        var report = new MortgageReport(calculator);
-        report.printMortgage();
-        report.printPaymentSchedule();
+         var calculator = new MortgageCalculator(principal,annualInterestRate,years);
+
+         var report = new MortgageReport(calculator);
+         report.printMortgage();
+         report.printPaymentSchedule();
+
     }
 
 }
