e408afbb58610c55834d6229eac0de414e9e2e55
Uma Thileepan <thileepan_uma@yahoo.co.uk>
2021-07-22 20:54:53 +0100

    User class has created to express Generic's CompareTo method.

---
+++ src/com/uma/Generics/User.java
@@ -0,0 +1,14 @@
+package com.uma.Generics;
+
+public class User implements Comparable<User>{
+
+    private int points;
+
+    public User(int points){
+        this.points = points;
+    }
+    @Override
+    public int compareTo(User other) {
+        return points - other.points;
+    }
+}
--- src/com/uma/Main.java
+++ src/com/uma/Main.java
@@ -1,19 +1,30 @@
 package com.uma;
 
-//import com.uma.Generics.GenericList;
-//import com.uma.Generics.List;
+import com.uma.Generics.GenericList;
+import com.uma.Generics.User;
 
 public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("Hello World");
 
-	    NumbersCalculation numbersCalculation = new NumbersCalculation();
-	    numbersCalculation.getOption();
-
-        /*var list = new GenericList<Integer>();
+        var list = new GenericList<Integer>();
         list.add(5);
-        list.add(10);*/
+        list.add(10);
+        int num1 = list.get(0);
+        System.out.println(num1);
+
+        var user1 = new User(50);
+        var user2 = new User(20);
+        if(user1.compareTo(user2) < 0)
+            System.out.println("User1 < User2");
+        else if(user1.compareTo(user2) == 0)
+            System.out.println("User1 == User2");
+        else
+            System.out.println("User1 > User2");
+
+	    /*NumbersCalculation numbersCalculation = new NumbersCalculation();
+	    numbersCalculation.getOption();
 
          int principal = (int) Console.readNumber("Principal($1K-1M): ", 1_000, 1_000_000);
          float annualInterestRate = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
@@ -23,7 +34,7 @@ public class Main {
 
          var report = new MortgageReport(calculator);
          report.printMortgage();
-         report.printPaymentSchedule();
+         report.printPaymentSchedule();*/
 
     }
 
